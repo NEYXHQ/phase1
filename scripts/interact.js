f27a23b32cfc6fe6d25580a9231d8eddd62150f1
AltoPal0 <161604323+AltoPal0@users.noreply.github.com>
2024-11-15 12:42:40 +0100

    working with no hardcoding - Not sure of resolution but has to do with sync of addresses of deployed contracts

--- scripts/interact.js
+++ scripts/interact.js
@@ -9,8 +9,11 @@ const { ethers } = hre;
 
 async function main() {
   // Specify deployed contract addresses
-  const tokenSaleAddress = "0x7bc06c482DEAd17c0e297aFbC32f6e63d3846650";
+  const tokenSaleAddress = "0x922D6956C99E12DFeB3224DEA977D0939758A1Fe";
   const mockAggregatorAddress = "0x7969c5eD335650692Bc04293B07F5BF2e7A673C0";
+
+  const myTokenAddress = "0x82e01223d51Eb87e16A03E24687EDF0F294da6f1";
+  const mockUSDCAddress = "0x2bdCC0de6bE1f7D2ee689a0342D76F52E8EFABa3";
 
   // Get deployer and buyer accounts
   const [deployer, buyer] = await ethers.getSigners();
@@ -27,19 +30,34 @@ async function main() {
   console.log("Attached MockV3Aggregator Address:", mockAggregator.target);
 
   // Set the USD / ETH pair price
-  await mockAggregator.updateAnswer(300000000000);
+  await mockAggregator.updateAnswer(350000000000);
 
   // Fetch the latest price using latestRoundData
-  const [, latestAnswer] = await mockAggregator.latestRoundData();
+  const result = await mockAggregator.latestRoundData();
+  const [, latestAnswer] = result;
+  console.log("Latest Answer:", latestAnswer.toString());
 
   // Convert the price from the int256 format with 8 decimals
   const price = parseFloat(ethers.formatUnits(latestAnswer, 8));
   console.log(`Current USD/ETH Price from Mock Aggregator: $${price}`);
 
+
+
+  // const SUYT2TokenSale = await ethers.getContractFactory("SUYT2TokenSale");
+  // const tokenSaleContract = await SUYT2TokenSale.deploy(
+  //   myTokenAddress,
+  //   mockUSDCAddress,
+  //   deployer.address,
+  //   mockAggregatorAddress
+  // );
+  // console.log("SUYT2TokenSale deployed to:", await tokenSaleContract.getAddress());
+
+
+
   const SUYT2TokenSale = await ethers.getContractFactory("SUYT2TokenSale");
   console.log(`1`);
   const tokenSaleContract = SUYT2TokenSale.attach(tokenSaleAddress);
-  console.log(`2`);
+  console.log(`2 - ${tokenSaleContract.target}`);
   // Fetch the latest ETH/USD price from the SUYT2TokenSale contract
   const latestPrice = await tokenSaleContract.getLatestETHPrice();
   console.log(`3`);
@@ -55,6 +73,8 @@ async function main() {
   // // Calculate the total cost in ETH for 1 token based on the USD price
   const tokenPriceInUSD = await tokenSaleContract.tokenPriceUSDC();
   console.log(`SUYT token price in USDC: $${ethers.formatUnits(tokenPriceInUSD, 6)} - ${tokenPriceInUSD}`);
+
+
   const totalCostInETH = await tokenSaleContract.getEquivalentETHAmount(tokenPriceInUSD);
   console.log(`SUYT token price in ETH based on agregator: $${ethers.formatUnits(totalCostInETH,18) }`);
   // // Convert totalCostInETH to a format for sending in the transaction
